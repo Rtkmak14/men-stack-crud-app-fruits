e524ba970a7075b13f23612fff48e6626b490c4b
Rtkmak14 <Rtkmak14@gmail.com>
2025-09-03 10:09:43 -0500

    express setup

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 //import express
 const express = require("express")
 const app = express()
-const PORT = 3000
+
+
+const dotenv = require("dotenv")
+dotenv.config()
+// console.log(process.env)
+const PORT = process.env.PORT
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)

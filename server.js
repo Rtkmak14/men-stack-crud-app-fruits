3d31604d97b1295bdc3e02f0c248f9c796ef81cc
Rtkmak14 <Rtkmak14@gmail.com>
2025-09-03 10:33:38 -0500

    created the fruit schema and model

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-//import express
-const express = require("express")
-const app = express()
-
-
+//load environment variables
 const dotenv = require("dotenv")
 dotenv.config()
 // console.log(process.env)
+
+//imports express
+const express = require("express")
+const app = express()
+
+//connect to mongoDB
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on("connected", ()=> {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
 const PORT = process.env.PORT
 
+//start server
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
+})
+
+//import model
+const Fruit = require(`./models/fruit.js`)
+
+//Routes
+app.get(`/`,(req,res)=> {
+    res.send("Hello, friend!")
 })
